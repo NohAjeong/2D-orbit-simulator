36f5d3f9ea9497e9afefce8a317ef4cade133895
NohAjeong <nohajeong@naver.com>
2026-09-07 12:43:57 +0900

    Separate acceleration calculation into reusable function, returning results via reference parameters

--- cpp/physics.cpp
+++ cpp/physics.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <cmath>
 
+void computeAcceleration(double x,double y,double GM, double& ax, double& ay){
+
+    double r = sqrt(pow(x,2)+ pow(y,2));
+    ax = -GM * x / pow(r,3);
+    ay = -GM * y / pow(r,3);
+
+}
+
 int main(){
 
     double x = 1.0;
@@ -8,13 +16,11 @@ int main(){
     double vx = 0.0;
     double vy = 1.5;
     const double GM = 1.0;
+    double ax;
+    double ay;
 
-    double r = sqrt(pow(x,2)+ pow(y,2));
-    double ax = -GM * x / pow(r,3);
-    double ay = -GM * y / pow(r,3);
-
+    computeAcceleration(x, y, GM, ax, ay);
     std::cout << "ax=" << ax << std::endl;
     std::cout << "ay=" << ay << std::endl;
-
     return 0;
 }
